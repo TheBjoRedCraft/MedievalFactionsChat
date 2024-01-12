4f975db210afcd1ae998e5f58689cdfe3946e142
Red <red.dev@hexle.at>
2024-01-12 18:20:12 +0100

    finished main stuff of the projekt

--- src/main/java/dev/thebjoredcraft/medievalfactionschat/ChatCMD.java
+++ src/main/java/dev/thebjoredcraft/medievalfactionschat/ChatCMD.java
@@ -7,7 +7,7 @@ import com.dansplugins.factionsystem.faction.MfFactionService;
 import com.dansplugins.factionsystem.player.MfPlayer;
 import com.dansplugins.factionsystem.player.MfPlayerService;
 import com.dansplugins.factionsystem.service.Services;
-import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -34,16 +34,17 @@ public class ChatCMD implements CommandExecutor {
 
                 MfFaction faction = factionService.getFactionByPlayerId(mfPlayer.getId());
                 if (faction == null) {
+                    player.sendMessage("Du bist in keiner Faction!");
                 } else {
                     if(args.length >=1) {
                         String msg = "";
-                        for( int i = 0; i < args.length; i++) {
-                            msg = msg + args[i]+" ";
-                            for(MfFactionMember member : faction.getMembers()){
-                                OfflinePlayer offlinePlayer = mfPlayer.toBukkit();
-                                Player targetMember = offlinePlayer.getPlayer();
-                                targetMember.sendMessage(msg);
-                            }
+                        for(int i = 0; i < args.length; i++) {
+                            msg = msg + args[i] + " ";
+                        }
+                        for(MfFactionMember member : faction.getMembers()){
+                            OfflinePlayer offlinePlayer = mfPlayer.toBukkit();
+                            Player targetMember = offlinePlayer.getPlayer();
+                            targetMember.sendMessage(MiniMessage.miniMessage().deserialize("<dark_gray>[<gradient:#3b92d1:#40d1db>Faction<dark_gray>]<reset> " + msg));
                         }
                     }else{
                         player.sendMessage("Bitte gib eine Nachricht ein.");
